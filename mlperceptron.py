c9ddf928ddee45c92ad6ed1030f223aef1cf9d69
Jaroslav Loebl <jaroslavloebl@gmail.com>
2018-03-07 17:45:49 +0100

    regression and classification notebooks with new version of pycgp

--- mlperceptron.py
+++
@@ -1,8 +0,0 @@
-from pycgp.benchmarks.symbolic import X, y
-from sklearn.metrics import mean_squared_error
-from sklearn.neural_network import MLPRegressor
-
-rgr = MLPRegressor(max_iter=100000, solver='lbfgs', hidden_layer_sizes=1000)
-print('start fit')
-rgr.fit(X, y)
-print(mean_squared_error(y, rgr.predict(X)))
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Genetic Programming presentation
+
+On Big Data seminar, at FIIT STU, 5.3.2018
+
+Files:
